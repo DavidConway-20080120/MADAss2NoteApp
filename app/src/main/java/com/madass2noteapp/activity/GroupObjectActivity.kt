d06712b1d986065d7e6765cf8d911b49364cbd0d
DavidConway-20080120 <20080120@mail.wit.ie>
2020-12-27 15:31:49 +0000

    group object nave functioning

--- app/src/main/java/com/madass2noteapp/activity/GroupObjectActivity.kt
+++ app/src/main/java/com/madass2noteapp/activity/GroupObjectActivity.kt
@@ -1,12 +1,36 @@
 package com.madass2noteapp.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.madass2noteapp.R
+import kotlinx.android.synthetic.main.activity_notetaker.*
+import kotlinx.android.synthetic.main.menu_group_.*
+import kotlinx.android.synthetic.main.menu_group_.button_back
+import kotlinx.android.synthetic.main.object_group.*
 
 class GroupObjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.object_group)
+
+        button_back.setOnClickListener{
+            val intent = Intent(this,
+                GroupMenuActivity::class.java)
+            startActivity(intent);
+            finish()
+        }
+        button_notes.setOnClickListener{
+            val intent = Intent(this,
+                NoteObjectActivity::class.java)
+            startActivity(intent);
+            finish()
+        }
+        button_delete.setOnClickListener{
+            val intent = Intent(this,
+                GroupMenuActivity::class.java)
+            startActivity(intent);
+            finish()
+        }
     }
 }
